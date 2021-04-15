78430a4382138ed49e5a769609349e80958f3703
Retch <retch100@gmail.com>
2021-04-15 23:09:05 +0200

    Updated README.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 
 setup(
     name='unofficialmailpoof',
-    version='0.0.1',
+    version='0.0.3',
     description='unofficial mailpoof module',
     long_description=open('README.md').read(),
     url='https://github.com/Retch/unofficial-mailpoof-python',
